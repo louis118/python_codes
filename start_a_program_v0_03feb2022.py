30e3c6e62562b9a49094626f36cb7655dc35bb23
louis118 <98979204+louis118@users.noreply.github.com>
2022-02-03 13:41:13 -0500

    Add files via upload

---
+++ start_a_program_v0_03feb2022.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 12:56:24 2022
+
+@author: louis118
+"""
+# this file shows how to run an office (word, excel, etc. ) using python
+
+import os
+#import webbrowser as wb
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+
+
+#driver = webdriver.Firefox()
+
+
+
+os.startfile("C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE")
+#os.startfile("C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE")
+#os.startfile("C:\\Program Files\\Microsoft Office\\root\\Office16\\POWERPNT.EXE")
+#os.startfile("C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE")
+
+
+#driver.get("http://www.python.org")
